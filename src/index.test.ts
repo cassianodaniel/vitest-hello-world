05be8d954c0e6675c616ed97ffe024414c786a1a
Daniel Chaves <daniel.cassiano@live.com>
2022-12-27 21:59:07 -0300

    hello-tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,67 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, test, assert } from 'vitest';
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
+});
+
+describe('string test', () => {
+	it('string test', () => {
+		expect('hello').toBe('hello');
+	});
+});
+
+describe('object test', () => {
+	it('object test', () => {
+		expect({ a: 1 }).toEqual({ a: 1 });
+	});
+});
+
+test('name', async () => {
+	/* ... */
+}, 1000);
+describe.skip('skipped suite', () => {
+	it('test', () => {
+		// Suite skipped, no error
+		assert.equal(Math.sqrt(3), 1.7320508075688772);
+	});
+});
+
+describe('suite', () => {
+	it.skip('skipped test', () => {
+		// Test skipped, no error
+		assert.equal(Math.sqrt(3), 1.7320508075688772);
+	});
+});
+
+// Only this suite (and others marked with only) are run
+describe.only('suite', () => {
+	it('test', () => {
+		assert.equal(Math.sqrt(3), 1.7320508075688772);
+	});
+});
+
+describe('another suite', () => {
+	it('skipped test', () => {
+		// Test skipped, as tests are running in Only mode
+		assert.equal(Math.sqrt(3), 1.7320508075688772);
+	});
+
+	it.only('test', () => {
+		// Only this test (and others marked with only) are run
+		assert.equal(Math.sqrt(3), 1.7320508075688772);
+	});
+});
+
+// An entry will be shown in the report for this suite
+describe.todo('unimplemented suite');
+
+// An entry will be shown in the report for this test
+describe('suite', () => {
+	it.todo('unimplemented test');
+});
+
+describe('test', () => {
+	assert.equal(3, 3);
 });
